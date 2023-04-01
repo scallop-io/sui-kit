be5c1e8aa09cc5ec41fcf6a54c5e660bf444ccd2
icefox <zengxinhai001@163.com>
2023-04-01 18:23:48 +0800

    Feat: Add example for transfer sui

---
+++ example/transfer-sui.ts
@@ -0,0 +1,36 @@
+/**
+ * This is an example of using SuiKit to transfer SUI from one account to another.
+ */
+import { SuiKit } from "../sui-kit";
+import * as process from "process";
+import dotenv from "dotenv";
+dotenv.config();
+
+(async() => {
+	const displayBalance = async (suiKit: SuiKit) => {
+		console.log(`balance for account ${suiKit.currentAddress()}: ${(await suiKit.getBalance()).totalBalance}`);
+	}
+
+	const mnemonics = process.env.MNEMONICS;
+
+	// Account that will receive SUI
+	const suiKitM = new SuiKit({ mnemonics, networkType: 'testnet' });
+	await displayBalance(suiKitM)
+
+	// Account that will send SUI
+	const secretKey = process.env.SECRET_KEY;
+	const suiKitS = new SuiKit({ secretKey, networkType: 'testnet' });
+	await displayBalance(suiKitS)
+
+	const gasBudget = 10**3 * 1200;
+	const balanceS = await suiKitS.getBalance();
+	console.log(`Transfer ${balanceS.totalBalance - gasBudget} from ${suiKitS.currentAddress()} to ${suiKitM.currentAddress()}`)
+	await suiKitS.transferSui(suiKitM.currentAddress(), balanceS.totalBalance - gasBudget)
+
+	console.log('Wait 3 seconds for the transaction to be confirmed...')
+	await new Promise(resolve => setTimeout(resolve, 3000));
+
+	console.log('After transfer:')
+	await displayBalance(suiKitM)
+	await displayBalance(suiKitS)
+})();

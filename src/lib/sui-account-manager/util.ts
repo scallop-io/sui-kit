c8bfaa1d2e7224e71ead2d1c76c043a521ffed55
icefox <zengxinhai001@163.com>
2023-04-08 11:08:17 +0800

    Add support for legacy private key

--- src/lib/sui-account-manager/util.ts
+++ src/lib/sui-account-manager/util.ts
@@ -44,19 +44,24 @@ export const hexOrBase64ToUint8Array = (str: string): Uint8Array => {
   }
 }
 
+const PRIVATE_KEY_SIZE = 32;
+const LEGACY_PRIVATE_KEY_SIZE = 64;
 /**
  * normalize a private key
  * A private key is a 32-byte array.
  * But there are two different formats for private keys:
  * 1. A 32-byte array
- * 2. A 33-byte array with the first byte being 0x00 (sui.keystore key is a Base64 string with scheme flag 0x00 at the beginning)
+ * 2. A 64-byte array with the first 32 bytes being the private key and the last 32 bytes being the public key
+ * 3. A 33-byte array with the first byte being 0x00 (sui.keystore key is a Base64 string with scheme flag 0x00 at the beginning)
  */
 export const normalizePrivateKey = (key: Uint8Array): Uint8Array => {
-  // sui.keystore key is a Base64 string with scheme flag 0x00 at the beginning
-  const PRIVATE_KEY_SIZE = 32;
-  if (key.length === PRIVATE_KEY_SIZE + 1 && key[0] === 0) {
+  if (key.length === LEGACY_PRIVATE_KEY_SIZE) {
+    // This is a legacy secret key, we need to strip the public key bytes and only read the first 32 bytes
+    key = key.slice(0, PRIVATE_KEY_SIZE);
+  } else if (key.length === PRIVATE_KEY_SIZE + 1 && key[0] === 0) {
+    // sui.keystore key is a Base64 string with scheme flag 0x00 at the beginning
     return key.slice(1);
-  } else if (key.length === 32) {
+  } else if (key.length === PRIVATE_KEY_SIZE) {
     return key;
   }
   throw new Error('invalid secret key');

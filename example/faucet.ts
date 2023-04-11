c9613118ab632145b305a38b8666d3a3ba84abf4
icefox.eth <zengxinhai001@163.com>
2023-04-11 20:35:19 +0800

    Remove the package related examples

--- example/faucet.ts
+++
@@ -1,9 +0,0 @@
-import * as dotenv from 'dotenv';
-import { SuiKit } from '../src';
-dotenv.config();
-
-(async () => {
-  const mnemonics = process.env.MNEMONICS;
-  const suiKit = new SuiKit({ mnemonics, networkType: 'testnet' });
-  await suiKit.requestFaucet({ accountIndex: 9 });
-})()

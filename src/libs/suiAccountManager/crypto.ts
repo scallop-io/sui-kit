7dd708557fb014246e0847995e23b7b612208230
Donnie <donor.gm@gmail.com>
2023-07-01 01:15:40 +0800

    chore: bump dependencies version

--- src/libs/suiAccountManager/crypto.ts
+++ src/libs/suiAccountManager/crypto.ts
@@ -1,5 +1,5 @@
 import { generateMnemonic as genMnemonic } from '@scure/bip39';
-import { wordlist } from '@scure/bip39/wordlists/english.js';
+import { wordlist } from '@scure/bip39/wordlists/english';
 
 export const generateMnemonic = (numberOfWords: 12 | 24 = 24) => {
   const strength = numberOfWords === 12 ? 128 : 256;

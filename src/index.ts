e7109e0324e6ffb028d2ab894d2859a2b79041af
fum-is-chum <alvinardiansyah2002@gmail.com>
2024-12-14 23:27:58 +0700

    feat: export sui interactor class

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ export { SuiKit } from './suiKit';
 export { SuiAccountManager } from './libs/suiAccountManager';
 export { SuiTxBlock } from './libs/suiTxBuilder';
 export { MultiSigClient } from './libs/multiSig';
+export { SuiInteractor } from './libs/suiInteractor';
 export type * from './types';

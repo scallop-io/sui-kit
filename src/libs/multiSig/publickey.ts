cf8b3e56499965463cba398f1dbd44b9175ee1bd
badukwei <linwgpeter@gmail.com>
2025-06-18 15:38:51 +0800

    feat: add coverage testing and update public key utility

--- src/libs/multiSig/publickey.ts
+++ src/libs/multiSig/publickey.ts
@@ -1,10 +1,10 @@
 import { PublicKey } from '@mysten/sui/cryptography';
 import { Ed25519PublicKey } from '@mysten/sui/keypairs/ed25519';
-import { fromB64 } from '@mysten/sui/utils';
+import { fromBase64 } from '@mysten/bcs';
 
 export function ed25519PublicKeyFromBase64(rawPubkey: string): PublicKey {
-  let bytes = fromB64(rawPubkey);
-  // rawPubkeys should either be 32 bytes or 33 bytes (with the first byte being flag)
+  let bytes = fromBase64(rawPubkey);
+  // raw public keys should either be 32 bytes or 33 bytes (with the first byte being flag)
   if (bytes.length !== 32 && bytes.length !== 33) throw 'invalid pubkey length';
   bytes = bytes.length === 33 ? bytes.slice(1) : bytes;
   return new Ed25519PublicKey(bytes);
